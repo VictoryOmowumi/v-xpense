c49a83d538cdf377eb32ed9c9dda883d5ec9021e
Victory omowumi Balogun <69681146+VictoryOmowumi@users.noreply.github.com>
2025-03-11 16:15:15 +0100

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,5 @@
 export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
-    dbCredentials: {
-      url:'postgresql://v-xpense_owner:LCaHPfnBN4s8@ep-divine-dream-a5fsdkb3.us-east-2.aws.neon.tech/v-xpense?sslmode=require',
-    }
+    
   };
